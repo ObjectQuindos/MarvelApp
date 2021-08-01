d321d184c035317069c6153bd3cd9a44dc5bb9a8
David Lopez Quindos <david.lopezquindos@plexus.es>
2021-08-02 00:58:56 +0200

    Creating project

---
+++ MarvelApp/MarvelApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelApp
+//
+//  Created by David López Quindos on 2/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
